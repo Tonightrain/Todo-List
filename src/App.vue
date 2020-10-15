5be2d76b63f48d7a1b850d7ea10bbc3a36200415
tonightrain <805560812@qq.com>
2020-10-15 17:47:45 +0800

    [feat]:显示主页

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,90 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="title">todos</h1>
+    <div class="todo-list">
+      <input class="todo" v-model="inputValue" placeholder="What needs to be done?" @keyup.enter="handleSubmit">
+      <ul class="lists">
+        <Todolist
+            v-for="(item,index) of list"
+            :key="index"
+            :content="item"
+            :index="index"
+            @delete="handleDelete"
+        >
+        </Todolist>
+      </ul>
+    </div>
+
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator';
+import Todolist from "@/components/Todolist.vue";
 
-export default {
-  name: 'App',
+@Component({
   components: {
-    HelloWorld
+    Todolist,
   }
+})
+export default class App extends Vue {
+  inputValue: string =  ''
+  list: string[] =  []
+
+  handleSubmit(): void {
+    this.list.push(this.inputValue);
+    this.inputValue = ''
+  }
+
+  handleDelete(index: number): void {
+    this.list.splice(index, 1)
+  }
+
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+#app{
+  background: #f5f5f5;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
+
+.title{
+  font-size: 90px;
+  font-weight: 100;
+  text-align: center;
+  color: rgba(175, 47, 47, 0.15);
+}
+
+.todo-list{
+  margin-top:-20px;
+}
+
+.todo{
+  outline: none;
+  display: block;
+  box-shadow: inset 0 -1px 0px #cccbcb;
+  padding: 10px 10px 10px 10px;
+  border: none;
+  width: 600px;
+  height: 50px;
+  margin-top:-50px;
+  background-color: white;
+}
+
+input::-webkit-input-placeholder{
+  color: #e8e7e7;
+}
+
+.todo, .edit {
+  font-size: 30px;
+  font-family: inherit;
+  font-weight: lighter;
+  font-style: italic;
+}
+
 </style>
---
+++ src/components/Todolist.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="todo-list">
+    <li @click="handleClick">{{content}}</li>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component({})
+export default class Todolist extends Vue {
+  @Prop() private index: undefined;
+  @Prop() private content!:string;
+
+  handleClick() {
+    this.$emit('delete',this.index)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
