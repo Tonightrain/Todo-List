1b0ffd3a7a8067fd13aa9558b53ac71b90d4f3ce
tonightrain <805560812@qq.com>
2020-10-16 10:22:29 +0800

    [feat]:删除事项

--- src/components/Todolist.vue
+++ src/components/Todolist.vue
@@ -1,7 +1,8 @@
 <template>
-  <li class="item" @click="handleClick">
-<!--    <span class="content">{{content}}</span>-->
+  <li class="item">
     {{content}}
+    <input type="checkbox" class="checkbox" v-model="checked">
+    <label class="delete-icon" @click="handleClick">×</label>
   </li>
 </template>
 
@@ -13,6 +14,8 @@ export default class Todolist extends Vue {
   @Prop() private index: undefined;
   @Prop() private content!:string;
 
+  checked:boolean = false
+
   handleClick() {
     this.$emit('delete',this.index)
   }
@@ -21,13 +24,62 @@ export default class Todolist extends Vue {
 
 <style scoped>
 .item{
-  padding: 10px 50px 10px 50px;
+  padding: 10px 0px 10px 50px;
   border: 1px solid white;
   border-bottom: 1px solid #e1dfdf;
   height: 45px;
   font-size: 30px;
   font-weight: lighter;
   color: #4d4d4d;
+}
+
+.item .delete-icon{
+  display: none;
+}
+
+.item:hover .delete-icon{
+  display: initial;
+}
+
+.delete-icon{
+  float: right;
+  margin-right: 30px;
+  color: #eea5b2;
+}
+
+.delete-icon:hover{
+  color: #c9687a;
+}
+
+.checkbox{
+  outline: none;
+  width:35px;
+  height:35px;
+  border:solid 1px #dddddd;
+  border-radius:50%;
+  float: left;
+  margin-left: -40px;
+  margin-right: 10px;
+  position:relative;
+  -webkit-appearance:none;
+}
+
+.checkbox:checked{
+  border:solid 1px #a8c5a8;
+}
+
+.checkbox:checked::after {
+  content:'';
+  top:9px;
+  left:6px;
+  position:absolute;
+  border: #7dc57d solid 2px;
+  border-top:none;
+  border-right:none;
+  height:8px;
+  width:18px;
+  -webkit-transform:rotate(-45deg);
+  transform:rotate(-45deg);
 }
 
 </style>
